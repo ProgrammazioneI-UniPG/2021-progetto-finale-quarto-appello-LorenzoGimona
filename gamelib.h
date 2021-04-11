19690545f35e55c5c42198a7ccde42bb199b8d4c
LorenzoGimona <82185697+LorenzoGimona@users.noreply.github.com>
2021-04-11 15:45:20 +0200

    Add files via upload

--- gamelib.h
+++ gamelib.h
@@ -1,5 +1,55 @@
-// Dichiarazione delle funzioni da chiamare in main.c
-// Definizione dei tipi necessari alla libreria
+//#define DEBUG_MAIN /* attiva codice debug di main */
+//#define DEBUG_IMPOSTA /*attiva codice debug imposta_gioco*/
+//#define DEBUG_GIOCA /*attiva codice debug gioca*/
+//#define DEBUG_DEALLOCA /*attiva codice debug dealloca*/
+//#define DEBUG_SET_STANZA /*attiva codice debug set_stanza*/
+//#define DEBUG_GIOCATORI /* attiva codice debug stampa_giocatori*/
 
 
+#include <stdbool.h>
+#include <stdio.h>
 
+#define random(x) rand() % x
+#define randomize srand((unsigned)time(NULL))
+#define ARANCIO "\x1b[38:5:208m"
+#define BISTRO "\x1b[38:5:239m"
+#define CIANO "\x1b[36m"
+#define FUCSIA "\x1b[38:5:198m"
+#define LILLA "\x1b[38:5:134m"
+#define MAGENTA "\x1b[35m"
+#define OCRA "\x1b[38:5:130m"
+#define SOLIDAGO "\x1b[38:5:142m"
+#define TURCHESE "\x1b[38:5:87m"
+#define VERMIGLIO "\x1b[38:5:124m"
+#define RED_ERROR "\x1b[38:5:9m"
+#define GREEN_INFO "\x1b[38:5:10m"
+#define RESET "\x1b[0m"
+
+enum Stato_giocatore {astronauta, impostore, assassinato, defenestrato};
+enum Nome_giocatore {arancio, bistro, ciano, fucsia, lilla, magenta, ocra, solidago, turchese, vermiglio};
+enum Tipo_stanza {vuota, quest_semplice, quest_doppia, botola};
+
+struct Stanza {
+    struct Stanza* avanti;
+    struct Stanza* sinistra;
+    struct Stanza* destra;
+    struct Stanza* stanza_precedente;
+    struct Stanza* ptr_next;
+    enum Tipo_stanza room_type;
+    _Bool emergenza_chiamata;
+  };
+
+struct Giocatore {
+    struct Stanza* position;
+    enum Stato_giocatore stato;
+    enum Nome_giocatore nome;
+  };
+
+// Funzioni non Static
+int imposta_gioco ();
+int gioca ();
+int termina_gioco ();
+int dealloca ();
+int leggi_riga (int max_num, char * riga);
+
+  _Bool gioco_impostato;
--- main.c
+++ main.c
@@ -1,13 +1,83 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 #include "gamelib.h"
 
-// Questo file continene solo la definizione della funzione main
-// con il menu principale 1-2-3
 
-int main( ) {
+int main()
+{
+  time_t t;
+  int iter;
+  int numero_caratteri;
+  int elementi;
+  int choice;
 
 
+  srand ((unsigned)time(&t));
 
+  gioco_impostato = false;
+
+#ifdef DEBUG_MAIN
+   printf("\nValore gioco_impostato: %d\n", gioco_impostato);
+#endif
+
+ do {
+  iter=0;
+  do {
+
+      iter++;
+      if (iter>1)
+       {
+        printf(RED_ERROR "\nRiprova: input non valido\n\n" RESET);
+       }
+
+      printf("\nMenu di gioco, per procedere premere:\n\n");
+      printf("1) imposta gioco\n");
+      printf("2) gioca\n");
+      printf("3) termina gioco\n");
+
+      elementi = 2;
+      char buffer[elementi];
+      numero_caratteri=leggi_riga(elementi, buffer);
+      choice=atoi(buffer);
+
+    } while ((choice<1)||(choice>3)||(numero_caratteri>2)||(numero_caratteri==0));
+
+#ifdef DEBUG_MAIN
+  printf("esco dal loop\n\n" );
+#endif
+
+  switch (choice)
+  {
+    case 1: if (gioco_impostato)
+             {
+              printf(GREEN_INFO"\nReimposto il gioco,,,,,,\n" RESET);
+              dealloca();
+              gioco_impostato = false;
+             }
+              imposta_gioco();
+              gioco_impostato=true;
+
+
+#ifdef DEBUG_MAIN
+              printf("gioco_impostato= %d\n", gioco_impostato);
+#endif
+              printf(GREEN_INFO"\nOra si può giocare......\n"RESET);
+              break;
+
+    case 2: if (!gioco_impostato)
+             {
+              printf(RED_ERROR "\nBisogna PRIMA impostare il gioco\n" RESET);
+              break;
+             }
+              gioca();
+// Dopo gioca si va in continuità al caso 3
+    case 3:   termina_gioco();
+              gioco_impostato=false;
+
+
+    }            //Fine switch
+  } while(true);
+ return 0;
 }
-
-
-
